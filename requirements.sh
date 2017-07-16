d6f3a00072f324af6664ba8b795d53817bb3ae85
goforbroke <go.for.broke1006@gmail.com>
2017-07-16 12:55:06 +0300

    Fix

--- requirements.sh
+++ requirements.sh
@@ -3,7 +3,9 @@
 composer install
 
 wget http://selenium-release.storage.googleapis.com/2.53/selenium-server-standalone-2.53.1.jar
-wget https://chromedriver.storage.googleapis.com/2.30/chromedriver_linux64.zip
-unzip -o chromedriver_linux64.zip && rm -f chromedriver_linux64.zip
+
+wget https://chromedriver.storage.googleapis.com/2.30/chromedriver_linux64.zip -O chromedriver.zip
+#wget https://chromedriver.storage.googleapis.com/2.30/chromedriver_win32.zip -O chromedriver.zip
+unzip -o chromedriver.zip && rm -f chromedriver.zip
 
 cp tests/_data/users.yml.dist tests/_data/users.yml
